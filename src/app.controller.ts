3e2a353471d113500b79bdd599952c62aa15bac9
YJ <cyj9721@gmail.com>
2021-08-27 23:17:51 +0900

    [Add] sayHello controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,13 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // express의 GET임
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/hello') // 13줄과 14줄 사이는 붙어
+  sayHello(): string {
+    return 'Hello everyone';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], // URL을 가져오고 함수를 실행하는 역할 ( express의 router 기능)
   providers: [AppService],
 })
 export class AppModule {}
