a080e2398ba240c0a53bafee922c5fa8960e42fa
YJ <cyj9721@gmail.com>
2021-08-27 23:33:30 +0900

    [Delete] delete controller, service file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController], // URL을 가져오고 함수를 실행하는 역할 ( express의 router 기능)
-  providers: [AppService],
+  controllers: [], // URL을 가져오고 함수를 실행하는 역할 ( express의 router 기능)
+  providers: [],
 })
 export class AppModule {}
