3cf648b4a57a3299d7a3b58cff5ceb3f5559a0a2
YJ <cyj9721@gmail.com>
2021-08-29 23:12:33 +0900

    [2.6] Modules and Dependency Injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController], // URL을 가져오고 함수를 실행하는 역할 ( express의 router 기능)
-  providers: [MoviesService],
+  imports: [MoviesModule],
+  controllers: [AppController], // URL을 가져오고 함수를 실행하는 역할 ( express의 router 기능)
+  providers: [],
 })
 export class AppModule {}
